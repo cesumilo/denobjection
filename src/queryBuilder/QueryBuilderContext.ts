93422db4ac651062ed303a09c6385de5c7c53233
Guillaume Robin <gdream.dev@gmail.com>
2024-07-07 19:29:33 +0200

    Add work for query builders

---
+++ src/queryBuilder/QueryBuilderContext.ts
@@ -0,0 +1,27 @@
+import { nany } from "../ninja.ts";
+import { QueryBuilderContextBase } from "./QueryBuilderContextBase.ts";
+
+export class QueryBuilderContext extends QueryBuilderContextBase {
+  runBefore: nany[];
+  runAfter: nany[];
+  onBuild: nany[];
+
+  constructor(builder?: nany) {
+    super(builder);
+
+    this.runBefore = [];
+    this.runAfter = [];
+    this.onBuild = [];
+  }
+
+  clone() {
+    const ctx = new QueryBuilderContext();
+    super.cloneInto(ctx);
+
+    ctx.runBefore = this.runBefore.slice();
+    ctx.runAfter = this.runAfter.slice();
+    ctx.onBuild = this.onBuild.slice();
+
+    return ctx;
+  }
+}
---
+++ src/queryBuilder/QueryBuilderUserContext.ts
@@ -0,0 +1,26 @@
+import { nany } from "../ninja.ts";
+import { Knex } from "knex";
+
+export class QueryBuilderUserContext {
+  #builder: nany;
+
+  constructor(builder: nany) {
+    this.#builder = builder;
+  }
+
+  get transaction(): Knex {
+    return this.#builder.knex();
+  }
+
+  newFromObject(builder: nany, obj: unknown): QueryBuilderUserContext {
+    const ctx = new QueryBuilderUserContext(builder);
+    Object.assign(ctx, obj);
+    return ctx;
+  }
+
+  newMerge(builder: nany, obj: unknown): QueryBuilderUserContext {
+    const ctx = new QueryBuilderUserContext(builder);
+    Object.assign(ctx, this, obj);
+    return ctx;
+  }
+}
